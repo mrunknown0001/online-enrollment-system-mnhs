c1b9d1e8525a1e3f7d561e088da8d5b52cd07f15
Michael Adam <madamt0001@gmail.com>
2019-06-11 08:39:34 +0800

    Update

--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -13,22 +13,22 @@
 @section('content')
     <div class="section-admin container-fluid">
         <div class="row">
-          <div class="col-md-12">
+          <div class="col-md-6">
             <br><br><br>
             <h1>Reports</h1>
 
             @include('includes.all')
 
-            <ul type="disc">
-              <li><a href="{{ route('admin.list.of.sections') }}">List of sections</a></li>
-              <li><a href="{{ route('admin.list.of.student.grade.level') }}">List of students per grade level</a></li>
-              <li><a href="{{ route('admin.list.of.student.per.section') }}">List of students per section</a></li>
-              <li><a href="{{ route('admin.reports.junior.high') }}">List of junior high students</a></li>
-              <li><a href="{{ route('admin.list.of.senior.high.students') }}">List of senior high students</a></li>
-              {{-- <li>List of faculties per grade level with sections assigned</li>
-              <li>List of faculties per department</li>
-              <li>List of class schedule</li> --}}
-            </ol>
+            
+              <p><a href="{{ route('admin.list.of.sections') }}" class="btn btn-primary btn-sm btn-block">List of sections</a></p>
+              <p><a href="{{ route('admin.list.of.student.grade.level') }}" class="btn btn-primary btn-sm btn-block">List of students per grade level</a></p>
+              <p><a href="{{ route('admin.list.of.student.per.section') }}" class="btn btn-primary btn-sm btn-block">List of students per section</a></p>
+              <p><a href="{{ route('admin.reports.junior.high') }}" class="btn btn-primary btn-sm btn-block">List of junior high students</a></p>
+              <p><a href="{{ route('admin.list.of.senior.high.students') }}" class="btn btn-primary btn-sm btn-block">List of senior high students</a></p>
+              {{-- List of faculties per grade level with sections assigned
+              List of faculties per department
+              List of class schedule --}}
+            
           </div>
         </div>
     </div>
