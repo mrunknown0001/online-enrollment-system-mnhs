029ec186f329780bf88c3840342c722059cd81cc
Michael Adam <madamt0001@gmail.com>
2019-06-13 09:49:53 +0800

    Update

---
+++ database/migrations/2019_06_13_094539_create_student_enrollment_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentEnrollmentHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_enrollment_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('student_section_id');
+            $table->foreign('student_section_id')->references('id')->on('student_sections');
+            $table->string('school_year', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_enrollment_histories');
+    }
+}
