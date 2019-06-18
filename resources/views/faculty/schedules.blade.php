324b8ec8221d29b28446c239de7ab6f7d0fe9d03
Michael Adam <madamt0001@gmail.com>
2019-06-18 20:49:10 +0800

    Update

--- resources/views/faculty/schedules.blade.php
+++ resources/views/faculty/schedules.blade.php
@@ -12,43 +12,45 @@
 
 @section('content')
 <div class="section-admin container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <br><br><br>
-            <h1>Schedules</h1>
-            <p>
-            	<button class="btn btn-primary" onclick="window.print()"><i class="fa fa-print"></i></button>
-            </p>
-            @include('includes.all')
+  <div class="row">
+    <div class="col-md-12">
+      <br><br><br>
+      <h1>Schedules</h1>
+      <p>
+      	<button class="btn btn-primary" onclick="window.print()"><i class="fa fa-print"></i></button>
+      </p>
+      @include('includes.all')
 
-            @if($schedules != NULL)
-                <table class="table table-hover table-bordered table-striped">
-                    <thead>
-                        <th>Section</th>
-                        <th>Subject</th>
-                        <th>Room</th>
-                        <th>Day</th>
-                        <th>Time</th>
-                    </thead>
-                    <tbody>
-                        @foreach($schedules as $s)
-                            <tr>
-                                <td>{{ $s['section'] }}</td>
-                                <td>{{ $s['subject'] }}</td>
-                                <td>{{ $s['room'] }}</td>
-                                <td>{{ $s['day'] }}</td>
-                                <td>
-                                    {{ $s['start_time'] }} - {{ $s['end_time'] }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @else 
-                <p class="text-center">NO Schedules Available.</p>
-            @endif
+      @if($schedules != NULL)
+        <div id="printArea">
+          <table class="table table-hover table-bordered table-striped">
+            <thead>
+              <th>Section</th>
+              <th>Subject</th>
+              <th>Room</th>
+              <th>Day</th>
+              <th>Time</th>
+            </thead>
+            <tbody>
+              @foreach($schedules as $s)
+                <tr>
+                  <td>{{ $s['section'] }}</td>
+                  <td>{{ $s['subject'] }}</td>
+                  <td>{{ $s['room'] }}</td>
+                  <td>{{ $s['day'] }}</td>
+                  <td>
+                    {{ $s['start_time'] }} - {{ $s['end_time'] }}
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
         </div>
+      @else 
+        <p class="text-center">NO Schedules Available.</p>
+      @endif
     </div>
+  </div>
 </div>
 @endsection
  
