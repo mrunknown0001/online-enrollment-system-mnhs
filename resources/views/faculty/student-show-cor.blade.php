014a5f0b28a02362b767c166b12ee59fecc89ff5
Michael Adam <madamt0001@gmail.com>
2019-06-21 12:48:51 +0800

    Update

--- resources/views/faculty/student-show-cor.blade.php
+++ resources/views/faculty/student-show-cor.blade.php
@@ -29,13 +29,13 @@
               <p class="text-center">
                 Republic of the Philippines
                 <br>
-                <img src="{{ asset('images/ched.png') }}" width="50px">
+                <img src="{{ asset('images/logo.png') }}" width="60px">
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 Maliwalo National High School
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <img src="{{ asset('images/deped.png') }}" width="50px">
+                <img src="{{ asset('images/deped.png') }}" width="60px">
                 <br>
                 Tarlac City
               </p>
