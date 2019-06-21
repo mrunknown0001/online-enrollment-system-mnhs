053b8b8e4930cc55a9b2b6f631e1d1ba44b7a926
Michael Adam <madamt0001@gmail.com>
2019-06-21 13:22:44 +0800

    Update

--- database/migrations/2019_01_01_090939_create_student_sections_table.php
+++ database/migrations/2019_01_01_090939_create_student_sections_table.php
@@ -20,6 +20,7 @@ class CreateStudentSectionsTable extends Migration
             $table->unsignedInteger('section_id');
             $table->foreign('section_id')->references('id')->on('sections');
             $table->unsignedInteger('assessor_id')->nullable();
+            $table->foreign('assessor_id')->references('id')->on('users');
             $table->tinyInteger('grade_level')->nullable();
             $table->string('semester', 15)->nullable();
             $table->boolean('active')->default(1);
